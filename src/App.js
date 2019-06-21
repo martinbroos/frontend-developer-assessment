250e4eab8091e252467a94e07d51e9b3d9b8ad9a
Martin Broos <martin.broos@freshheads.com>
2019-06-21 15:15:59 +0200

    remove logo to create a better hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { ReactComponent as Logo } from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo className="App-logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+      <br />
+      <a
+        href="https://facebook.github.io/create-react-app/docs/getting-started"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Read more about create-react-app
+      </a>
     </div>
   );
 }
